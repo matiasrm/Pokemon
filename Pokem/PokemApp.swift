0ea4a9eb8835e21b2e87de2927c40f6f6dcd1842
Matias Vidoni <matiasrmontenegro@gmail.com>
2021-10-03 15:04:04 -0300

    Initial Commit

---
+++ Pokem/PokemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemApp.swift
+//  Pokem
+//
+//  Created by Mati Montenegro on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
